9a4b30a9393e4441b1e5a857c02bf68dbb8a9109
cmanfred3 <chowder3>
2017-05-21 21:37:15 -0400

    ok

---
+++ app/src/main/java/com/example/android/easternathletics/SoccerBoysSchedule.java
@@ -0,0 +1,13 @@
+package com.example.android.easternathletics;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SoccerBoysSchedule extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soccer_boys_schedule);
+    }
+}
